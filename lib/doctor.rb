078c679f0f4d21e4ab530d862cdc04249b3903be
Mitchel Parish <mitchelparish@gmail.com>
2019-02-03 18:09:04 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,6 +11,17 @@ class Doctor
     @@all
   end
 
+  def new_appointment(patient, date)
+    appointment = Appointment.new(date, self, doctor)
+    @appointments << appointment
+    appointment
+  end
 
+  def appointments
+  end
 
+  def patients
+  end 
+
+  
 end
